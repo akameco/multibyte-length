11c383b15f7eb808d1166e93671f0bb5f0044515
akameco <akameco.t@gmail.com>
2017-04-06 16:14:12 +0900

    Add yarn & Update deps

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ module.exports = function (str) {
 		throw new TypeError('Expected a string');
 	}
 
-	var escapedStr = escape(str);
+	const escapedStr = escape(str);
 
-	var len = 0;
-	for (var i = 0; i < escapedStr.length; i++, len++) {
+	let len = 0;
+	for (let i = 0; i < escapedStr.length; i++, len++) {
 		if (escapedStr.charAt(i) === '%') {
 			if (escapedStr.charAt(++i) === 'u') {
 				i += 3;
